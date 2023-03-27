a74cb51f89dd95e3e1b5ab4b079d815c3901bbf2
Andrian <kogoshvili@pm.me>
2023-03-27 18:19:09 +0400

    fixed path resolution logic, \ added srcPrefix, \ fixed default nodeModulesPrefix, \ changed default migrations

--- src/index.js
+++ src/index.js
@@ -1,73 +1,117 @@
 const fs = require('fs');
 const path = require('path');
-const exec = require("child_process").exec;
+const exec = require('child_process').exec;
 const loaderUtils = require('loader-utils');
 
 module.exports = function (source, map, meta) {
-  const rootDir = this.rootContext;
-  const callback = this.async();
-  const options = this?.getOptions?.() || loaderUtils.getOptions(this);
-  const { migrations, flags, nodeModulesPrefix } = options;
+    const rootDir = this.rootContext;
+    const callback = this.async();
+    const options = this?.getOptions?.() || loaderUtils.getOptions(this);
 
-  const sourcePath = this.resourcePath;
-  const sourceExt = path.extname(sourcePath);
+    const {
+        migrations,
+        flags,
+        nodeModulesPrefix = '~',
+        srcPrefix = 'app'
+    } = options;
 
-  const content = importContent(source, sourcePath, rootDir, nodeModulesPrefix);
-  const tempFile = path.resolve(path.join(__dirname, '..', `./temp.${sourceExt}`));
+    const sourcePath = this.resourcePath;
+    const sourceExt = path.extname(sourcePath);
 
-  fs.writeFileSync(tempFile, content);
+    const content = importContent(source, sourcePath, rootDir, nodeModulesPrefix, srcPrefix);
+    const cleanContent = content.replace(/[\u200B-\u200D\uFEFF]/g, '');
 
-  migrate(tempFile, migrations, flags).then(() => {
-    const buffer = fs.readFileSync(tempFile);
-    const newContent = buffer.toString('utf8');
-    fs.unlinkSync(tempFile);
-    callback(null, newContent, map, meta);
-  });
-}
+    const tempFile = path.resolve(path.join(__dirname, '..', `./temp.${sourceExt.startsWith('.') ? sourceExt.slice(1) : sourceExt}`));
 
-function importContent(source, sourcePath, rootDir, nodeModulesPrefix) {
-  const regex = new RegExp(/@(import|use)( ?)[\"'](.+)[\"'];/g);
-  let match;
-  let content = source;
+    fs.writeFileSync(tempFile, cleanContent);
 
-  while (match = regex.exec(source)) {
-    let importPath = match[3];
+    migrate(tempFile, migrations, flags).then(() => {
+        const buffer = fs.readFileSync(tempFile);
+        const newContent = buffer.toString('utf8');
+        callback(null, newContent, map, meta);
+    });
+};
 
-    if (importPath.startsWith(nodeModulesPrefix)) {
-      importPath = importPath.replace(nodeModulesPrefix, `${rootDir}/node_modules/`);
+function importContent(source, sourcePath, rootDir, nodeModulesPrefix, srcPrefix) {
+    const regex = new RegExp(/@(import|use)( ?)[\"'](.+)[\"'];/g);
+    let match;
+
+    let content = source;
+
+    while (match = regex.exec(source)) {
+        let importPath = match[3];
+
+        if (importPath.startsWith(srcPrefix)) {
+            importPath = importPath.replace(srcPrefix, `${rootDir}/src/`);
+        } else if (importPath.startsWith(`${nodeModulesPrefix}${srcPrefix}`)) {
+            importPath = importPath.replace(`${nodeModulesPrefix}${srcPrefix}`, `${rootDir}/src/`);
+        } else if (importPath.startsWith('src')) {
+            importPath = importPath.replace('src', `${rootDir}/src/`);
+        } else if (importPath.startsWith(nodeModulesPrefix)) {
+            importPath = importPath.replace(nodeModulesPrefix, `${rootDir}/node_modules/`);
+        } else if (importPath.startsWith('@')) {
+            importPath = `${rootDir}/node_modules/${importPath}`;
+        }
+
+        const importFullPath = path.resolve(path.dirname(sourcePath), importPath);
+        let filePath = '';
+
+        if (!fs.existsSync(importFullPath)) {
+            const fileDir = path.dirname(importFullPath);
+            const fileExt = path.extname(importFullPath);
+            const fileName = path.basename(importFullPath, fileExt);
+
+            const fileExtensions = fileExt ? [fileExt] : ['.scss', '.sass', '.css'];
+            const fileNames = [fileName, `_${fileName}`];
+
+            for (const ext of fileExtensions) {
+                for (const name of fileNames) {
+                    const candidatePath = path.join(fileDir, `${name}${ext}`);
+                    if (fs.existsSync(candidatePath)) {
+                        filePath = candidatePath;
+                        break;
+                    }
+                }
+                if (filePath) {
+                    break;
+                }
+            }
+        } else {
+            filePath = importFullPath;
+        }
+
+        let importSource = '';
+
+        importSource = fs.readFileSync(filePath, 'utf8');
+
+        const nestedContent = importContent(importSource, filePath, rootDir, nodeModulesPrefix, srcPrefix);
+        content = content.replace(match[0], nestedContent);
     }
-
-    const importFullPath = path.resolve(path.dirname(sourcePath), importPath);
-    const importSource = fs.readFileSync(importFullPath, 'utf8');
-    const nestedContent = importContent(importSource, importFullPath, rootDir, nodeModulesPrefix);
-    content = content.replace(match[0], nestedContent);
-  }
-
-  return content;
+    return content;
 }
 
 async function migrate(filepath, migrations = defaultMigrations, flags = []) {
-  const promises = migrations.map(migration => migrator(filepath, migration, flags));
-  return Promise.all(promises);
+    const promises = migrations.map(migration => migrator(filepath, migration, flags));
+    return Promise.all(promises);
 }
 
 async function migrator(filepath, migration, flags = []) {
-  return new Promise((resolve, reject) => {
-    const flagsString = flags.join(' ');
-    exec(`sass-migrator ${migration} ${flagsString} ${filepath}`, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(stdout);
-      }
+    return new Promise((resolve, reject) => {
+        const flagsString = flags.join(' ');
+        exec(`sass-migrator ${migration} ${flagsString} ${filepath}`, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(stdout);
+            }
+        });
     });
-  })
 }
 
 const defaultMigrations = [
-  'division', // Use the math.div() function instead of the / division operator
-  'media-logic', // Migrates deprecated `@media` query syntax.\nSee https://sass-lang.com/d/media-logic.
-  'module', // Use the new module system.
-  'namespace', // Change namespaces for `@use` rules.
-  'strict-unary' // Migrates deprecated `$a -$b` syntax (and similar) to unambiguous `$a - $b`
-];
+    'division', // Use the math.div() function instead of the / division operator
+    // 'media-logic', // Migrates deprecated `@media` query syntax.\nSee https://sass-lang.com/d/media-logic.
+    // 'module', // Use the new module system.
+    // 'namespace', // Change namespaces for `@use` rules.
+    // 'strict-unary' // Migrates deprecated `$a -$b` syntax (and similar) to unambiguous `$a - $b`
+  ];
